d85718fedd69eaaf85b6d34bacf999858894d6e8
Dmitry Rzhenev <rzhenev.116@gmail.com>
2025-11-11 00:59:10 +0300

    feat: complete signUp frontend implementation

--- frontend/src/pages/auth/SignUpPage.jsx
+++ frontend/src/pages/auth/SignUpPage.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
-import AuthPage from "../../components/common/auth/AuthPage.jsx";
+import {AuthContext} from "../../context/AuthContext.js";
+import {useNavigate} from "react-router-dom";
+import {useAuth, useAuthForm, useSubmit} from "../../hooks/index.js";
+import {decodeJWT} from "../../utils/index.js";
 import {SIGNUP_FIELDS} from "../../constants/authForms.js";
-import {useAuthForm} from "../../hooks/index.js";
+import AuthPage from "../../components/common/auth/AuthPage.jsx";
 
 const SignUpPage = () => {
-    const { formData, handleInputChange } = useAuthForm({
-        username: '',
-        email: '',
-        password: '',
-    })
+    const { login } = useAuth(AuthContext);
+    const navigate = useNavigate();
 
-    const handleSubmit = () => {}
+    const {formData, handleInputChange} = useAuthForm({
+        username: '',
+        password: ''
+    });
+
+    const { loading, error, signUp, resetError } = useSubmit();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const result = await signUp(formData);
+        if (result.success) {
+            /** @type {{accessToken: string, refreshToken: string}} */
+            const userData = decodeJWT(result.data.accessToken);
+            login(result.data, userData);
+            navigate('/dashboard');
+        }
+    }
+
+    const handleInputChangeWithErrorReset = (fieldName, value) => {
+        handleInputChange(fieldName, value);
+        resetError();
+    };
 
     return (
         <AuthPage
             title="Регистрация"
             fieldsConfig={SIGNUP_FIELDS}
             formData={formData}
-            onInputChange={handleInputChange}
+            onInputChange={handleInputChangeWithErrorReset}
             onSubmit={handleSubmit}
-            submitText="Зарегистрироваться"
+            submitText={loading ? "Регистрация..." : "Зарегистрироваться"}
             linkPath="/signin"
             linkText="Есть аккаунт?"
+            errorMessage={error}
+            disabled={loading}
         />
     );
 };
